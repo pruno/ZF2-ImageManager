30439dd2032b0a96411ad8df060bda536d6538f1
Pruno <luca.bruno.it@gmail.com>
2013-09-27 15:41:42 +0200

    Setup composer

---
+++ Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ImageManager;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array('Zend\Loader\StandardAutoloader' => array(
+            'namespaces' => array(
+                __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+            ),
+        ));
+    }
+}
